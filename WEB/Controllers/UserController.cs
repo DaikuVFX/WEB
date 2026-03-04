34478ab1b40eaf3b50f337f2454515adff042296
Sebastian Kuška <kuskas24@spstrutnov.cz>
2026-03-04 10:33:21 +0100

    funguje prihlaseni

---
+++ WEB/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WEB.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /User (profil po přihlášení)
+        [Authorize]
+        public IActionResult Index()
+        {
+            var username = User.FindFirst(ClaimTypes.Name)?.Value;
+            ViewBag.Username = username;
+            return View();
+        }
+
+        // GET: /User/Register
+        public IActionResult Register()
+        {
+            return RedirectToAction("Register", "Account");
+        }
+
+        // GET: /User/Login
+        public IActionResult Login()
+        {
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
