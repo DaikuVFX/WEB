0f8d48201fcbd01c2c83729720342236f4bac144
Sebastian Kuška <kuskas24@spstrutnov.cz>
2026-03-12 09:34:25 +0100

    zobravovatelske jmeno

--- WEB/Controllers/UserController.cs
+++ WEB/Controllers/UserController.cs
@@ -1,27 +1,49 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using WEB.Data;
+using WEB.Models;
 
 namespace WEB.Controllers
 {
     public class UserController : Controller
     {
-        // GET: /User (profil po přihlášení)
+        private readonly AppDbContext _db;
+        public UserController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         [Authorize]
         public IActionResult Index()
         {
             var username = User.FindFirst(ClaimTypes.Name)?.Value;
+            var user = _db.Users.FirstOrDefault(u => u.Username == username);
             ViewBag.Username = username;
-            return View();
+            return View(user);
         }
 
-        // GET: /User/Register
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Index(string displayName)
+        {
+            var username = User.FindFirst(ClaimTypes.Name)?.Value;
+            var user = _db.Users.FirstOrDefault(u => u.Username == username);
+            if (user != null)
+            {
+                user.DisplayName = displayName;
+                await _db.SaveChangesAsync();
+                ViewBag.Success = "Změny uloženy!";
+            }
+            ViewBag.Username = username;
+            return View(user);
+        }
+
         public IActionResult Register()
         {
             return RedirectToAction("Register", "Account");
         }
 
-        // GET: /User/Login
         public IActionResult Login()
         {
             return RedirectToAction("Login", "Account");
